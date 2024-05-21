5d85fb01126ffd7e28fb71e37ee743fcbeebf9f5
elaus <elaus00@gmail.com>
2024-05-21 10:43:34 +0900

    Feature : 멀티 마커 기능 구현

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,34 +1,45 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom"; // React Router Dom for navigation
 import styles from "./Map.module.css";
 import profile from "../Profile/Profile.jsx";
-import CustomMapMarker from "./Marker/CustomMapMarker.jsx"
-
+import CustomMapMarker from "./Marker/CustomMapMarker.jsx";
+import { addMarkers } from "./Marker/Marker.jsx"; // markers.js 파일에서 함수 import
 
 const MapNaverCur = () => {
   const mapElement = useRef(null);
   const { naver } = window;
   const [myLocation, setMyLocation] = useState({ latitude: 0, longitude: 0 });
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const navigate = useNavigate(); // Hook for navigation
 
-  //윈도우 사이즈에 따라 viewportWidth 설정
   useEffect(() => {
     const handleResize = () => {
       setViewportWidth(window.innerWidth);
     };
- 
-    window.addEventListener('resize', handleResize);
- 
+
+    window.addEventListener("resize", handleResize);
+
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
- 
+
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error);
     }
   }, []);
 
+  function success(position) {
+    setMyLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+
+  function error() {
+    setMyLocation({ latitude: 37.4979517, longitude: 127.0276188 });
+  }
   useEffect(() => {
     if (
       !mapElement.current ||
@@ -47,12 +58,10 @@ const MapNaverCur = () => {
       center: location,
       zoom: 17,
       zoomControl: true,
-      // 슬라이더 있으려면 스타일을 LARGE로. 없으면 SMALL
       zoomControlOptions: {
         style: naver.maps.ZoomControlStyle.SMALL,
         position: naver.maps.Position.RIGHT_CENTER,
       },
-      // 네이버 로고 위치 변경. 없애는 것은 네이버 정책상 불가.
       logoControlOptions: {
         position: naver.maps.Position.LEFT_BOTTOM,
       },
@@ -61,35 +70,38 @@ const MapNaverCur = () => {
     };
 
     const map = new naver.maps.Map(mapElement.current, mapOptions);
+
+    /* 마커 구현 
+       이 데이터를 백에서 받아와야 함 */
+    const totalDataArray = [
+      { dom_id: "1", title: "Marker 1", lat: 37.6543, lng: 127.0560 },
+      { dom_id: "2", title: "Marker 2", lat: 37.6550, lng: 127.0570 },
+      { dom_id: "3", title: "Marker 3", lat: 37.6615, lng: 127.0604 },
+      { dom_id: "4", title: "Marker 4", lat: 37.6468, lng: 127.0473 },
+      { dom_id: "5", title: "Marker 5", lat: 37.6501, lng: 127.0647 },
+      { dom_id: "6", title: "Marker 6", lat: 37.6579, lng: 127.0495 }
+    ];
+    
+    // 임시로 만든 마커 하나
     const name = "잘빠진 메밀";
-    // 기본 마커 추가
+
     const marker = new naver.maps.Marker({
       position: location,
       map: map,
-      title : name,
+      title: name,
       icon: {
-        content : CustomMapMarker({ title: name, windowWidth: viewportWidth }),
-         //마커의 크기 지정
+        content: CustomMapMarker({ title: name, windowWidth: viewportWidth }),
         size: new naver.maps.Size(38, 58),
-        //마커의 기준위치 지정
         anchor: new naver.maps.Point(19, 58),
-      }
-    }); 
+      },
+    });
 
+    const windowWidth = window.innerWidth;
+
+    addMarkers(naver, map, totalDataArray, windowWidth);
   }, [myLocation, naver]);
 
   return <div ref={mapElement} style={{ minHeight: "100vh" }} />;
-
-  function success(position) {
-    setMyLocation({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
-  }
-
-  function error() {
-    setMyLocation({ latitude: 37.4979517, longitude: 127.0276188 });
-  }
 };
 
 export default MapNaverCur;
--- src/pages/Pureplate/Header/header.jsx
+++ src/pages/Pureplate/Header/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./header.module.css"; 
+import styles from "./header.module.css";
 import SearchBar from "../../../components/Search/SearchBar.js";
 import Attributes from "../../../components/DietAttributes/DietAttributes.js";
 import Profile from "../../../components/Profile/Profile.jsx";
@@ -7,10 +7,14 @@ import Profile from "../../../components/Profile/Profile.jsx";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.purePlateIcon} />
-      <SearchBar />
-      <Attributes />
-      <Profile />
+      <div className={styles.left}>
+        <div className={styles.purePlateIcon} />
+        <SearchBar />
+      </div>
+      <div className={styles.right}>
+        <Attributes />
+        <Profile />
+      </div>
     </header>
   );
 };
